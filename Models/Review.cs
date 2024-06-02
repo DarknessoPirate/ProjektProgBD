c4cbf0646a3e5849ebe8ded404879c882f837050
Filip Wójcik <fw308054@student.polsl.pl>
2024-06-02 18:42:57 +0200

    initiall commit

---
+++ Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektProgBD.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Content { get; set; }
+
+        public User User { get; set; }
+        public int UserId { get; set; }
+
+        public Game Game { get; set; }
+        public int GameId { get; set; }
+    }
+}
