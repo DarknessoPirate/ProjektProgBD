a3311c7d91cd98f95505f2c16f76a9aebd1223c2
Darknesso <darknesso.steam@tlen.pl>
2024-06-02 18:44:38 +0200

    config tools creation

---
+++ Tools/ConfigurationSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektProgBD.Tools
+{
+    public static class ConfigurationSetup
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection, configuration);
+
+            return serviceCollection.BuildServiceProvider();
+        }
+
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(configuration);
+            services.AddDbContext<DBContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<MainWindow>();
+            // Add other services and dependencies here
+        }
+    }
+}
