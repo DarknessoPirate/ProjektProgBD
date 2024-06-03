51db5ae754772cb7c636cde19016c56c5a681774
Darknesso <darknesso.steam@tlen.pl>
2024-06-03 21:44:31 +0200

    added registration window, registrationWindowViewModel, bindings in views, new functions in viewmodels, moved views to separate folder

---
+++ ViewModels/RegistrationWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using ProjektProgBD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ProjektProgBD.ViewModels
+{
+    public class RegistrationWindowViewModel :ViewModelBase
+    {
+        #region private properties
+        private string _userName;
+
+        #endregion
+
+        #region accessors
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                onPropertyChanged(nameof(UserName));
+            }
+        }
+        #endregion
+
+        #region commands
+        private ICommand registerCommand;
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                if (registerCommand == null)
+                    registerCommand = new RelayCommand(
+                        parameter => Register(parameter),
+                        predicate => true // /// // / // / / / / / / / pomyślec nad predykatem
+                        );
+
+                return registerCommand;
+            }
+        }
+        #endregion
+
+        #region functions
+        private string GetPassword(object parameter)
+        {
+            if (parameter is PasswordBox passwordBox)
+            {
+                return passwordBox.Password.ToString();
+            }
+            return "";
+        }
+
+        private void Register(object parameter)
+        {
+            MessageBox.Show($"Username:{_userName}\nPassword:{GetPassword(parameter)}");
+        }
+        #endregion
+
+    }
+}
