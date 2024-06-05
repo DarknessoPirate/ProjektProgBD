2b0f9266c870137ea594806f636415aaf7fb7ac2
Filip Wójcik <fw308054@student.polsl.pl>
2024-06-05 18:38:58 +0200

    klucz_zlozony_migracja

--- Migrations/20240605163814_klucz_zlozony.Designer.cs
+++ Migrations/20240605163814_klucz_zlozony.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjektProgBD.Models;
 
@@ -11,9 +12,11 @@ using ProjektProgBD.Models;
 namespace ProjektProgBD.Migrations
 {
     [DbContext(typeof(ShopDbContext))]
-    partial class ShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605163814_klucz_zlozony")]
+    partial class klucz_zlozony
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
