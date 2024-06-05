b630c73a6e4748ee14c8b55644e4ab2703a64751
Darknesso <darknesso.steam@tlen.pl>
2024-06-05 19:50:53 +0200

    Added Game creation tab in the navigation for users "Darknesso" and "filip"

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -107,7 +107,16 @@ namespace ProjektProgBD.ViewModels
                 Tabs.Add(new TabItem { Header = "Shop", Content = new ShopView(), DataContext = new ShopViewModel(currentUser) });
                 Tabs.Add(new TabItem { Header = "Reviews", Content = new ReviewsView(), DataContext = new ReviewsViewModel(currentUser) });
                 Tabs.Add(new TabItem { Header = "Profile", Content = new ProfileView(), DataContext = new ProfileViewModel(currentUser) });
-                
+                /*
+                 * 
+                 * DEBUGGING PURPOSES ONLY
+                 * DELETE AFTER USED
+                 * 
+                 */
+                if (currentUser.Name == "Darknesso" || currentUser.Name == "filip")
+                {
+                    Tabs.Add(new TabItem { Header = "GameAdd", Content = new GameAddView(), DataContext = new GameAddViewModel() });
+                }
                 MessageBox.Show("Logged in :)");
             }
             else
