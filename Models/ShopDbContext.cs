61c02835f5711e1286e1dfde2a9b40c3593e2ad1
Darknesso <darknesso.steam@tlen.pl>
2024-06-10 20:20:34 +0200

    shopdbcontext removed unnecessary constructor

--- Models/ShopDbContext.cs
+++ Models/ShopDbContext.cs
@@ -13,10 +13,6 @@ namespace ProjektProgBD.Models
 {
     public class ShopDbContext : DbContext
     {
-        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
-        {
-
-        }
         public ShopDbContext()
         {
         }
