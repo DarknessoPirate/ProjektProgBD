16b6bb5a4927b1efec86bd1f0ccdf5c62aff69ab
Darknesso <darknesso.steam@tlen.pl>
2024-06-10 20:49:24 +0200

    added getgamereviews

--- Repositories/RepositoryReview.cs
+++ Repositories/RepositoryReview.cs
@@ -100,5 +100,21 @@ namespace ProjektProgBD.Repositories
             }
             return list;
         }
+
+        public static ObservableCollection<Review> GetGameReviews(Game game)
+        {
+            var list = new ObservableCollection<Review>();
+            using (var db = new ShopDbContext())
+            {
+                var reviews = db.Reviews.Where(u => u.GameId == game.Id).Include(g => g.User).Include(g => g.Game).ToList();
+                foreach (var review in reviews)
+                {
+                    list.Add(review);
+                }
+            }
+
+            return list;
+        }
+
     }
 }
