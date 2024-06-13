eba2d524940414ea39c1619972cce75d0f9ed695
Darknesso <darknesso.steam@tlen.pl>
2024-06-13 19:58:55 +0200

    Added images folder

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,8 +1,9 @@
 ﻿
 
 using ProjektProgBD.Models;
+using System.Net.Http;
 using System.Windows.Threading;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using Newtonsoft.Json.Linq;
 
 namespace ProjektProgBD.ViewModels
 {
@@ -26,6 +27,9 @@ namespace ProjektProgBD.ViewModels
         private User _currentUser;
         private string _currentDate;
         private string _currentTime;
+        private string _weatherCondition;
+        private string _weatherImage;
+        private double _temperature;
         private DispatcherTimer _timer;
         #endregion
 
@@ -64,6 +68,37 @@ namespace ProjektProgBD.ViewModels
                 onPropertyChanged(nameof(CurrentTime));
             }
         }
+
+        public string WeatherCondition
+        {
+            get => _weatherCondition;
+            set
+            {
+                _weatherCondition = value;
+                onPropertyChanged(nameof(WeatherCondition));
+            }
+        }
+        public string WeatherImage
+        {
+            get => _weatherImage;
+            set
+            {
+                _weatherImage = value;
+                onPropertyChanged(nameof(WeatherImage));
+            }
+        }
+
+        public double Temperature
+        {
+            get => _temperature;
+            set
+            {
+                _temperature = value;
+                onPropertyChanged(nameof(Temperature));
+            }
+        }
+
+
         #endregion
 
         #region commands
@@ -74,12 +109,55 @@ namespace ProjektProgBD.ViewModels
         {
             CurrentTime = DateTime.Now.ToString("HH:mm");
         }
+
+        public async Task GetWeatherDataAsync()
+        {
+            string apiKey = "YOUR_API_KEY";
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q=YOUR_CITY_NAME&appid={apiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    JObject weatherData = JObject.Parse(result);
+                    WeatherCondition = weatherData["weather"][0]["main"].ToString();
+                    Temperature = Convert.ToDouble(weatherData["main"]["temp"]);
+                    UpdateWeatherImage(WeatherCondition);
+                }
+            }
+        }
+
+        private void UpdateWeatherImage(string condition)
+        {
+            switch (condition.ToLower())
+            {
+                case "clear":
+                    WeatherImage = "Images/clear.png";
+                    break;
+                case "clouds":
+                    WeatherImage = "Images/clouds.png";
+                    break;
+                case "rain":
+                    WeatherImage = "Images/rain.png";
+                    break;
+                case "snow":
+                    WeatherImage = "Images/snow.png";
+                    break;
+                default:
+                    WeatherImage = "Images/default.png";
+                    break;
+            }
+        }
+
+
         #endregion
 
 
 
 
 
-        
+
     }
 }
